45550434e883e504576c5de0a1893d6b51a41f61
Rodrigo Viana <rodviana92@gmail.com>
2014-06-12 14:23:04 -0500

    modificacion catalogos permisos

--- CRMQualium/application/views/catalogo_permisos.php
+++ CRMQualium/application/views/catalogo_permisos.php
@@ -5,42 +5,29 @@
         </script>
         <script type="text/javascript" src="<?=base_url().'css/bootstrap-3.1.1-dist/js/transition.js'?>">
         </script>
-		<section id="catalogoPermisos">			
-
-		    <div class="contenido_permisos">
-			  	<h3>Nuevo Permiso</h3>
-				<hr class="linea_x">
-				<form id="registroPermiso">
-	        		<div>
-						<input id="permiso" type="text"  name="nombre" class="form-control" placeholder="Nombre del permiso">
-						<textarea id="comentario" name="comentario" class="form-control" rows="4" placeholder="Comentarios"></textarea>
-					</div>
-					<div style="margin-top:15px; margin-left:0px;">
-						<button id="guardar" type="button" class="btn btn-primary">Guardar</button>
-			       	    <button type="button" class="btn btn-default">Cancelar</button>
-		            </div>
-			    </form>
-		    </div>
-		    <div class="contenido_permisos">
-				<div class="panel panel-default" style="margin-top: 25px; width:600px; ">
-					<div class="panel-heading">
-					  <h4 class="panel-title">
-					    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-					      <b>Lista de permisos</b>
-					      <span class=" icon-uniF48B flecha_abajo"></span> 
-					    </a>
-					  </h4>
-					</div>
-					<div id="collapse1" class="panel-collapse collapse">
-					  	<div class="panel-body">
-					  		<table class="table table-hover">
-		        				<tbody id="scroll_permisos">																									
-								</tbody>									
-							</table>
-	      			 	</div>
-	      			</div>
-	      		</div>							    	      			        
-			</div>							
+		<section id="catalogoPermisos">	   
+		  	<h3>Nuevo Permiso</h3>
+			<hr>
+			<form id="registroPermiso">
+        		
+					<input id="permiso" type="text"  name="nombre" class="form-control" placeholder="Nombre del permiso" style="width:85%; display: inline-block;">
+					<!-- <textarea id="comentario" name="comentario" class="form-control" rows="4" placeholder="Comentarios"></textarea> -->
+				
+				<div style="display: inline-block;">
+					<button id="guardar" type="button" class="btn btn-primary">Guardar</button>
+		       	    <button type="button" class="btn btn-default">Cancelar</button>
+	            </div>
+		    </form>
+		    <h3>Lista de permisos</h3>
+			<hr>
+			<input type="search" class="form-control" placeholder="Busqueda" style="width:100%; ">
+	        <span id="busqueda_rol" class="glyphicon glyphicon-search"></span>  					
+		  	<div class="panel panel-primary" style="width:100%;">
+		  		<table class="table table-hover">
+    				<tbody id="scroll_permisos">																									
+					</tbody>									
+				</table>
+			</div>	        
 		</section>			
      </section> <!--Esta fin de sección pertenece al menú principal del contenido de esta página. -->
 </div>
@@ -48,7 +35,7 @@
     <script type="text/javascript" src="<?=base_url().'js/funcionescrm.js'?>"></script>
 <script type = "text/plantilla" id="listarPermisos">
 
-	<td style="width: 560px; " class="icon-operaciones"><label for="<%- id %>"><%- nombre %></label>
+	<td style="width: 1200px; " class="icon-operaciones"><label for="<%- id %>"><%- nombre %></label>
 	<div class="eliminar_permiso">
 		<span class="icon-trash" id="" data-toggle="tooltip" title="Eliminar"></span>
 		<span class="icon-edit" id="" data-toggle="tooltip"  title="Editar"></span>										
